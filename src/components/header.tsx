e88e08010091c17086b636483268eb3fdd35279a
Nicolas Pedranti <nicopedranti@gmail.com>
2025-01-21 00:42:41 -0300

    chore: change width of logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <>
       <header className="mb-8 flex items-center gap-3 px-4">
-        <img src="logo.webp" alt="cat logo" className="h-auto w-8" />
+        <img src="logo.webp" alt="cat logo" className="h-auto w-10" />
         {/* <Receipt className="size-8 text-blue-600" /> */}
         <h1 className="text-3xl font-bold text-gray-900">Cuentitas</h1>
       </header>
